7a94c2ac0c72906a47ca2d7e2a5e90b7e551a9f8
Richard Darst <rkd@zgib.net>
2020-03-15 14:40:43 +0200

    Revise tests, check in-script detection

--- nbscript/test_nbscript.py
+++ nbscript/test_nbscript.py
@@ -12,8 +12,8 @@ import time
 
 import pytest
 
-import nbscript
-nbscript.LOG.setLevel(logging.DEBUG)
+from nbscript.nbscript import nbscript, LOG as nbscript_LOG
+nbscript_LOG.setLevel(logging.DEBUG)
 
 
 @contextlib.contextmanager
@@ -39,13 +39,13 @@ def assert_out(fname):
 
 def test_basic_stdout(tdir, capfd):
     """Test notebook run on stdout"""
-    nbscript.nbscript(['one.ipynb'])
+    nbscript(['one.ipynb'])
     captured = capfd.readouterr()
     assert '0123456789' in captured.out
 
 def test_basic_save(tdir):
     """Test notebook run on stdout"""
-    nbscript.nbscript(['-o', 'out.md', 'one.ipynb'])
+    nbscript(['-o', 'out.md', 'one.ipynb'])
     assert_out('out.md')
 
 @pytest.mark.parametrize("fmt,output",
@@ -56,16 +56,37 @@ def test_basic_save(tdir):
                          ])
 def test_save(tdir, fmt, output):
     to = ['--to', fmt] if fmt else []
-    nbscript.nbscript(to + ['--save', 'one.ipynb'])
+    nbscript(to + ['--save', 'one.ipynb'])
     assert_out(output)
 
 def test_timestamp_save(tdir):
-    nbscript.nbscript(['--save', '--timestamp', 'one.ipynb'])
+    nbscript(['--save', '--timestamp', 'one.ipynb'])
     print(os.listdir('.'))
     output = glob.glob('*.md')[0]
     assert time.strftime('%Y-%m-%d') in output
 
 def test_timestamp_filename(tdir):
-    nbscript.nbscript(['--out', 'out.ipynb', '--timestamp', 'one.ipynb'])
+    nbscript(['--out', 'out.ipynb', '--timestamp', 'one.ipynb'])
     output = glob.glob('out*')[0]
     assert time.strftime('%Y-%m-%d') in output
+
+def test_detect_not_running(tdir):
+    import nbscript
+    assert not nbscript.argv, "Running detected but we shouldn't be"
+
+def test_detect_running(tdir, capfd):
+    nbscript(['one.ipynb'])
+    captured = capfd.readouterr()
+    assert 'Yes running nbscript' in captured.out
+
+
+@pytest.mark.parametrize("argv",
+                         [('A', 'B'),
+                          ('A', '--B'),
+                          ('--A', 'B'),
+                          ('--A', '--B'),
+                         ])
+def test_argv(tdir, capfd, argv):
+    nbscript(['one.ipynb'] + list(argv))
+    captured = capfd.readouterr()
+    assert " ".join(argv) in captured.out
